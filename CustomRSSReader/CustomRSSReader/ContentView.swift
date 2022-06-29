6aa353191644c9c1fc7a151176e9f62d907b8897
Mario Hernandez <mherncor@everis.com>
2022-06-29 16:54:06 +0200

    Initial SwiftUI project

---
+++ CustomRSSReader/CustomRSSReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomRSSReader
+//
+//  Created by Mario Hernandez Corral on 29/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Custom RSS Reader")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomRSSReader/CustomRSSReader/CustomRSSReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRSSReaderApp.swift
+//  CustomRSSReader
+//
+//  Created by Mario Hernandez Corral on 29/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomRSSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
